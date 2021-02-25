d16a33a228d2f1336916f7fcfe574e4e66cf1d5e
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-02-25 17:20:09 +0100

    add adminServer to the microservices environment.

---
+++ curso-microservice-infraestructuradomain/curso-microservice-adminserver/src/main/java/com/paymentchain/adminserver/CursoMicroserviceAdminServerApplication.java
@@ -0,0 +1,29 @@
+package com.paymentchain.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration
+@EnableAdminServer
+public class CursoMicroserviceAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroserviceAdminServerApplication.class, args);
+	}
+	
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		}
+	}
+}
